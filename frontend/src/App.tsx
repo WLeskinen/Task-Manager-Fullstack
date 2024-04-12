047991ac32867e630ee4f74a01d89784b7384de8
Waltteri Leskinen <58485176+WLeskinen@users.noreply.github.com>
2024-04-12 12:45:41 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+      </p>
+    </>
+  )
+}
+
+export default App
