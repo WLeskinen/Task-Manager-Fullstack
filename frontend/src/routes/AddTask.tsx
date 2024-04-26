d0fc4e48979160e0ff440544687802204678a23f
WLeskinen <58485176+WLeskinen@users.noreply.github.com>
2024-04-26 16:59:29 +0300

    Added buttons when adding a task

--- frontend/src/routes/AddTask.tsx
+++ frontend/src/routes/AddTask.tsx
@@ -24,115 +24,130 @@ const AddTask: React.FC<AddTaskProps> = ({ newTask, setNewTask, setTasks }) => {
             console.error("Error adding task:", error);
         }
     };
-        return (
-            newTask && (
-                <div>
-                  <h2>Add New Task</h2>
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      handleAddTask(newTask);
-                    }}
-                  >
-                    <div>
-                      <label>
-                        <strong>Name</strong>
-                        <input
-                          type="text"
-                          value={newTask.name || ''}
-                          onChange={(e) =>
-                            setNewTask({
-                              ...newTask,
-                              name: e.target.value,
-                            })
-                          }
-                        />
-                      </label>
-                    </div>
-                    <div>
-                      <label>
-                        <strong>Content</strong>
-                        <textarea
-                          value={newTask.content || ''}
-                          onChange={(e) =>
-                            setNewTask({
-                              ...newTask,
-                              content: e.target.value,
-                            })
-                          }
-                        />
-                      </label>
-                    </div>
-                    <div>
-                      <label>
-                        <strong>Start Date</strong>
-                        <input
-                          type="date"
-                          value={newTask.startDate ? newTask.startDate.toDateString().split('T')[0] : ''}
-                          onChange={(e) =>
-                            setNewTask({
-                              ...newTask,
-                              startDate: new Date(e.target.value),
-                            })
-                          }
-                        />
-                      </label>
-                    </div>
-                    <div>
-                      <label>
-                        <strong>End Date</strong>
-                        <input
-                          type="date"
-                          value={newTask.endDate ? newTask.endDate.toDateString().split('T')[0] : ''}
-                          onChange={(e) =>
-                            setNewTask({
-                              ...newTask,
-                              endDate: new Date(e.target.value),
-                            })
-                          }
-                        />
-                      </label>
-                    </div>
-                    <div>
-                  <label>
-                    <strong>Status</strong>
-                    <select
-                      value={newTask.status || ''}
-                      onChange={(e) =>
-                        setNewTask({
-                          ...newTask,
-                          status: e.target.value,
-                        })
-                      }
-                    >
-                      <option value="Not Started">Not Started</option>
-                      <option value="In Progress">In Progress</option>
-                      <option value="Completed">Completed</option>
-                    </select>
-                  </label>
-                </div>
-                    <div>
-                      <label>
-                        <strong>Tags</strong>
-                        <input
-                          type="text"
-                          value={newTask.tags || ''}
-                          onChange={(e) =>
-                            setNewTask({
-                              ...newTask,
-                              tags: e.target.value.split(','),
-                            })
-                          }
-                        />
-                      </label>
-                    </div>
-                    <button type="submit">Add Task</button>
-                  </form>
-                  <button onClick={() => setNewTask(null)}>Cancel</button>
-                </div>
-              )
-          );
-        }
+    return (
+      newTask && (
+        <div>
+          <h2 className="">Add New Task</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleAddTask(newTask);
+            }}
+          >
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>Name</strong>
+                <input
+                  type="text"
+                  value={newTask.name || ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      name: e.target.value,
+                    })
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>Content</strong>
+                <textarea
+                  value={newTask.content || ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      content: e.target.value,
+                    })
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>Start Date</strong>
+                <input
+                  type="date"
+                  value={newTask.startDate ? newTask.startDate.toDateString().split('T')[0] : ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      startDate: new Date(e.target.value),
+                    })
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>End Date</strong>
+                <input
+                  type="date"
+                  value={newTask.endDate ? newTask.endDate.toDateString().split('T')[0] : ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      endDate: new Date(e.target.value),
+                    })
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>Status</strong>
+                <select
+                  value={newTask.status || ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      status: e.target.value,
+                    })
+                  }
+                >
+                  <option value="Not Started">Not Started</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Completed">Completed</option>
+                </select>
+              </label>
+            </div>
+            <div>
+              <label className="font-bold font-ebrima text-primary">
+                <strong>Tags</strong>
+                <input
+                  type="text"
+                  value={newTask.tags || ''}
+                  onChange={(e) =>
+                    setNewTask({
+                      ...newTask,
+                      tags: e.target.value.split(','),
+                    })
+                  }
+                />
+              </label>
+            </div>
+            
+            {/* Buttons div */}
+            <div className="flex justify-start items-center p-4">
+              <button
+                type="submit"
+                className="font-bold font-ebrima p-3 rounded-lg mr-4 relative overflow-hidden hover:border-red-500 border-2 transition-all duration-200"
+              >
+                Add Task
+              </button>
+              <button
+                onClick={() => setNewTask(null)}
+                className="font-bold font-ebrima p-3 rounded-lg relative overflow-hidden hover:border-red-500 border-2 transition-all duration-200"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )
+    );
+    
+}
 
 
 export default AddTask;
