7ec87b1d71bdaea5580bbff17bc265ccd09366e5
WLeskinen <58485176+WLeskinen@users.noreply.github.com>
2024-04-26 17:13:13 +0300

    Stylization added for adding a new task

--- frontend/src/routes/AddTask.tsx
+++ frontend/src/routes/AddTask.tsx
@@ -27,104 +27,112 @@ const AddTask: React.FC<AddTaskProps> = ({ newTask, setNewTask, setTasks }) => {
     return (
       newTask && (
         <div>
-          <h2 className="">Add New Task</h2>
+          <h2 className="text-2xl font-bold text-primary py-4 font-ebrima">Add New Task</h2>
           <form
             onSubmit={(e) => {
               e.preventDefault();
               handleAddTask(newTask);
             }}
           >
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>Name</strong>
-                <input
-                  type="text"
-                  value={newTask.name || ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      name: e.target.value,
-                    })
-                  }
-                />
-              </label>
-            </div>
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>Content</strong>
-                <textarea
-                  value={newTask.content || ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      content: e.target.value,
-                    })
-                  }
-                />
-              </label>
-            </div>
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>Start Date</strong>
-                <input
-                  type="date"
-                  value={newTask.startDate ? newTask.startDate.toDateString().split('T')[0] : ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      startDate: new Date(e.target.value),
-                    })
-                  }
-                />
-              </label>
-            </div>
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>End Date</strong>
-                <input
-                  type="date"
-                  value={newTask.endDate ? newTask.endDate.toDateString().split('T')[0] : ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      endDate: new Date(e.target.value),
-                    })
-                  }
-                />
-              </label>
-            </div>
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>Status</strong>
-                <select
-                  value={newTask.status || ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      status: e.target.value,
-                    })
-                  }
-                >
-                  <option value="Not Started">Not Started</option>
-                  <option value="In Progress">In Progress</option>
-                  <option value="Completed">Completed</option>
-                </select>
-              </label>
-            </div>
-            <div>
-              <label className="font-bold font-ebrima text-primary">
-                <strong>Tags</strong>
-                <input
-                  type="text"
-                  value={newTask.tags || ''}
-                  onChange={(e) =>
-                    setNewTask({
-                      ...newTask,
-                      tags: e.target.value.split(','),
-                    })
-                  }
-                />
-              </label>
+            <div className="flex flex-wrap mb-4">
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>Name</strong>
+                  <input
+                    type="text"
+                    value={newTask.name || ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        name: e.target.value,
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  />
+                </label>
+              </div>
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>Content</strong>
+                  <textarea
+                    value={newTask.content || ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        content: e.target.value,
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  />
+                </label>
+              </div>
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>Start Date</strong>
+                  <input
+                    type="date"
+                    value={newTask.startDate ? newTask.startDate.toDateString().split('T')[0] : ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        startDate: new Date(e.target.value),
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  />
+                </label>
+              </div>
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>End Date</strong>
+                  <input
+                    type="date"
+                    value={newTask.endDate ? newTask.endDate.toDateString().split('T')[0] : ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        endDate: new Date(e.target.value),
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  />
+                </label>
+              </div>
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>Status</strong>
+                  <select
+                    value={newTask.status || ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        status: e.target.value,
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  >
+                    <option value="Not Started">Not Started</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                  </select>
+                </label>
+              </div>
+              <div className="w-1/2 px-2">
+                <label className="font-bold font-ebrima text-primary block mb-2">
+                  <strong>Tags</strong>
+                  <input
+                    type="text"
+                    value={newTask.tags || ''}
+                    onChange={(e) =>
+                      setNewTask({
+                        ...newTask,
+                        tags: e.target.value.split(','),
+                      })
+                    }
+                    className="border-black border rounded p-2 w-full"
+                  />
+                </label>
+              </div>
             </div>
             
             {/* Buttons div */}
