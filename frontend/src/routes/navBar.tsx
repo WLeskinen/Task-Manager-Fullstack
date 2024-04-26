1b24c1e5af935876230f60a59f854c55c8cdfc19
WLeskinen <58485176+WLeskinen@users.noreply.github.com>
2024-04-26 14:17:12 +0300

    Added navBar stylization

--- frontend/src/routes/navBar.tsx
+++ frontend/src/routes/navBar.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="bg-navBar text-text font-corbel items-center px-4" style={{ fontSize: '30px' }}>
-      <Link to="/" className="hover:text-slash mr-7">Home</Link>
-      <Link to="/tasks" className="hover:text-slash mr-7">Tasks</Link>
-      <Link to="/activities" className="hover:text-slash mr-7">Activities</Link>
-      <Link to="/statistics" className="hover:text-slash mr-7">Statistics</Link>
+    <nav className="bg-navBar text-text font-corbel items-center px-4 py-5" style={{ fontSize: '30px' }}>
+      <Link to="/" className="hover:text-slash mr-7 transition-colors duration-300">Home</Link>
+      <Link to="/tasks" className="hover:text-slash mr-7 transition-colors duration-300">Tasks</Link>
+      <Link to="/activities" className="hover:text-slash mr-7 transition-colors duration-300">Activities</Link>
+      <Link to="/statistics" className="hover:text-slash mr-7 transition-colors duration-300">Statistics</Link>
     </nav>
   );
 };
